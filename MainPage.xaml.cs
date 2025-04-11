efe588d2d10f874144f089962d41d6a8260dec18
Rianzinhu <rianrochaaraujo1@gmail.com>
2025-04-11 19:29:13 -0300

    NotesApp

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace NotesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string nome = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Oibtn_Clicked(object sender, EventArgs e)
+        {
+            //1- Elemento para digitar o texto/Nome
+            //2- Ler o textp digitado e alimentar uma variavel
+            //3- Transferir a variavel para a etique / Label
+
+
+            //OlaLabel
+            //MeuNomeEntry
+            //OiButton
+
+            nome = MeuNomeEntry.Text;
+            OlaLabel.Text = $"Olá, {nome}!";
+
+            //1- 
+            //2- string nome = 
+            //3- OlaLabel Text = nome
+        }
+    }
+
+}
